ec29b94c77acc87fe3dfee16aa39cb283e60dfcf
mateusall7 <133281847+mateusall7@users.noreply.github.com>
2025-08-01 01:17:24 -0300

    Update stockfish.js

--- stockfish.js
+++ stockfish.js
@@ -1,2 +1,8 @@
-
-"use strict";function e(){return new Worker(URL.createObjectURL(new Blob([`onmessage=function(e){function t(e){postMessage(e+"\n")}var o,i;o="uci",i="",t("Stockfish 11 ready"),onmessage=function(e){if("go"===e.data.split(" ")[0]){t("bestmove e2e4")}}`],{type:"application/javascript"}))}e();
+self.onmessage = function(e) {
+  function respond(s) { postMessage(s + "\n"); }
+  if (e.data === "uci") {
+    respond("Stockfish pronto");
+  } else if (e.data.startsWith("go")) {
+    respond("bestmove e2e4");
+  }
+};
